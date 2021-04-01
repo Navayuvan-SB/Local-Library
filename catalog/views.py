d7c21fb2f3128516465777a09598ed95b0db1016
navayuvansb <sb.navayuvan@gmail.com>
2021-04-01 15:53:06 +0530

    Add view logic for home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, BookInstance, Author, Genre
+
+
+def index(request):
+    """View function for home page of site"""
+
+    total_number_of_books = Book.objects.count()
+    total_number_of_book_instance = Book.objects.count()
+
+    total_number_of_available_books = BookInstance.objects.filter(
+        status__exact='a').count()
+
+    total_number_of_authors = Author.objects.count()
+
+    total_number_of_genre = Genre.objects.filter(name__contains='sci').count()
+
+    context = {
+        'total_number_of_books': total_number_of_books,
+        'total_number_of_book_instance': total_number_of_books,
+        'total_number_of_available_books': total_number_of_books,
+        'total_number_of_authors': total_number_of_books,
+        'total_number_of_genre': total_number_of_genre
+    }
+
+    return render(request, 'index.html', context=context)
