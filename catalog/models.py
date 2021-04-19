600f4acca3ff70bb2e72d48ea9930d6b143d11ea
navayuvansb <sb.navayuvan@gmail.com>
2021-04-19 11:35:48 +0530

    Add number of pages field to Book model

--- catalog/models.py
+++ catalog/models.py
@@ -108,6 +108,8 @@ class Book(models.Model):
     genre = models.ManyToManyField(
         Genre, help_text='Select a genre for this book')
 
+    number_of_pages = models.IntegerField(default=None, null=True)
+
     language = models.ForeignKey('Language', on_delete=models.SET, null=True)
 
     def __str__(self):
