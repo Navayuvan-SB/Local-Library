f0cb4f5dc56f1b3db36a3a2aeb749d3038e57293
navayuvansb <sb.navayuvan@gmail.com>
2021-04-20 12:29:37 +0530

    Add test cases for Publisher List View

--- catalog/tests/tests_views.py
+++ catalog/tests/tests_views.py
@@ -567,3 +567,40 @@ class GenreCreateViewTest(TestCase):
 
         self.assertRedirects(response, reverse(
             'genre-detail', kwargs={'pk': 1}))
+
+
+class PublisherListViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        number_of_publishers = 14
+        for publisher_id in range(number_of_publishers):
+            Publisher.objects.create(name=f'Publisher {publisher_id}')
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/catalog/publishers/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(reverse('publishers'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('publishers'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/publisher_list.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('publishers'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'])
+        self.assertTrue(len(response.context['publisher_list']) == 10)
+
+    def test_lists_all_authors(self):
+        response = self.client.get(reverse('publishers') + '?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'])
+        self.assertTrue(len(response.context['publisher_list']) == 4)
